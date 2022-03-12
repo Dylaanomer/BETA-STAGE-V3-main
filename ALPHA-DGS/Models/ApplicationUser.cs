2526d41d11d5b396a011cf9d3bbe35270798544f
Dylaanomer <70571414+Dylaanomer@users.noreply.github.com>
2022-03-12 01:58:40 +0100

    Gebruiker Lijst toegevoegd

--- ALPHA-DGS/Models/ApplicationUser.cs
+++ ALPHA-DGS/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,5 +13,10 @@ namespace ALPHA_DGS.Models
         [Required]
 
         public string Name { get; set; }
+        [NotMapped]
+        public string RoleId { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; }
     }
 }
