53c927115916757abdc549e65058b4ac765f44ce
Dylaanomer <70571414+Dylaanomer@users.noreply.github.com>
2022-03-12 02:40:33 +0100

    Role Beheer Toegevoegd

---
+++ ALPHA-DGS/Controllers/RolesController.cs
@@ -0,0 +1,50 @@
+﻿using ALPHA_DGS.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALPHA_DGS.Controllers
+{
+   
+        public class RolesController : Controller
+        {
+            private readonly AlphaDbContext _db;
+            private readonly UserManager<IdentityUser> _userManager;
+            private readonly RoleManager<IdentityRole> _roleManager;
+
+
+            public RolesController(AlphaDbContext db, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+            {
+                _db = db;
+                _roleManager = roleManager;
+                _userManager = userManager;
+            }
+
+
+            public IActionResult Index()
+            {
+                var roles = _db.Roles.ToList();
+                return View(roles);
+            }
+
+            [HttpGet]
+            public IActionResult Upsert(string id)
+            {
+                if (String.IsNullOrEmpty(id))
+                {
+                    return View();
+                }
+                else
+                {
+                    //update
+                    var objFromDb = _db.Roles.FirstOrDefault(u => u.Id == id);
+                    return View(objFromDb);
+                }
+
+
+            }
+        }
+}
