77ef0d60f119f64b0af4ca586c4cfda8241f9b61
Dylaanomer <70571414+Dylaanomer@users.noreply.github.com>
2022-03-14 02:26:19 +0100

    User Edit

--- ALPHA-DGS/Controllers/UserController.cs
+++ ALPHA-DGS/Controllers/UserController.cs
@@ -22,13 +22,13 @@ namespace ALPHA_DGS.Controllers
         }
 
 
-        public IActionResult Index ()
+        public IActionResult Index()
 
         {
             var userList = _db.ApplicationUser.ToList();
             var userRole = _db.UserRoles.ToList();
             var roles = _db.Roles.ToList();
-            foreach(var user in userList)
+            foreach (var user in userList)
             {
                 var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
                 if (role == null)
@@ -43,8 +43,28 @@ namespace ALPHA_DGS.Controllers
             }
 
             return View(userList);
+        }
 
-
+        public IActionResult Edit(string userId)
+        {
+            var objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == userId);
+            if (objFromDb == null)
+            {
+                return NotFound();
+            }
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            var role = userRole.FirstOrDefault(u => u.UserId == objFromDb.Id);
+            if (role != null)
+            {
+                objFromDb.RoleId = roles.FirstOrDefault(u => u.Id == role.RoleId).Id;
+            }
+            objFromDb.RoleList = _db.Roles.Select(u => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(objFromDb);
         }
     }
 }
